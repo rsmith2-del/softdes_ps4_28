b36bb0984f002644f6f3f0557b3ebaf30b10ba21
Robert Smith <rsmith2_olin_edu@cpu002.unity.rc.umass.edu>
2026-03-12 02:06:02 +0000

    testing function 3 for real this time

--- 4-using-sharing-code/gene_finder.py
+++ 4-using-sharing-code/gene_finder.py
@@ -4,12 +4,8 @@ Library for finding potential genes in a strand of DNA.
 
 import helpers
 
-COMPLIMENT_DICT = {
-    'A' : 'T',
-    'T' : 'A',
-    'C' : 'G',
-    'G' : 'A'
-}
+COMPLIMENT_DICT = {"A": "T", "T": "A", "C": "G", "G": "A"}
+
 
 def get_complement(nucleotide):
     """
@@ -34,38 +30,49 @@ def get_reverse_complement(strand):
     Returns:
         the reversed compliment of the given strand of DNA
     """
-    reverse_compliment = ''
+    reverse_compliment = ""
     for nucleotide in strand:
         reverse_compliment = reverse_compliment + get_complement(nucleotide)
     return reverse_compliment[::-1]
-    
 
 
 def rest_of_orf(strand):
     """
-    Your docstring goes here.
+    Returns an orf, a strand of nucleotides from a start codon to an end codon.
+
+    Args:
+    strand- a string of nucleotides
+
+    Returns:
+    final_strand- a string of nucleotides that describes an orf
+
+
     """
     from textwrap import wrap
-   #breaks strand into a list of 3 character long strings
-    i=0
-    final_strand = ""
-    if len(strand)%3 == 0:
-        chunk_strand = (wrap(strand, width=3))
-    else:
-        return 'Incorrect strand length'
 
-    if strand[0] == 'ATG':
+    # breaks strand into a list of 3 character long strings
+    i = 0
+    final_strand = ""
+    if len(strand) % 3 == 0:
+        chunk_strand = wrap(strand, width=3)
+        print(chunk_strand)
+        print(chunk_strand[0])
+    else:
+        return ""
+
+    if chunk_strand[0] == "ATG":
         while i < len(chunk_strand):
-            if chunk_strand[i] == 'TAA':
+            if chunk_strand[i] == "TAA":
                 return final_strand
-            if chunk_strand[i] == 'TAG':
+            if chunk_strand[i] == "TAG":
                 return final_strand
-            if chunk_strand[i] == 'TGA':
+            if chunk_strand[i] == "TGA":
                 return final_strand
             else:
                 final_strand = final_strand + (chunk_strand[i])
-            i+=1
-
+            i += 1
+        return ""
+    return ""
 
 
 def find_all_orfs_one_frame(strand):
